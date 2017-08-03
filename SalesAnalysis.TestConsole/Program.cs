d187dc37027d6ae064bbd3a06b74e938083d2b06
Monika Wróbel <wrobelmonika000@gmail.com>
2017-08-03 13:27:10 +0200

    Add project files.

---
+++ SalesAnalysis.TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesAnalysis.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+
+            Console.WriteLine("Stop");
+            Console.ReadLine();
+        }
+    }
+}
